92f65384fecb17d03f018f247872b766f9443863
Jule Jerrish Quijano <julr789@gmail.com>
2024-10-18 13:47:37 +0800

    [ADDED CATEGORY BACKEND (updated)]

--- ASI.Basecode.Services/Services/CategoryService.cs
+++ ASI.Basecode.Services/Services/CategoryService.cs
@@ -34,21 +34,6 @@ namespace ASI.Basecode.Services.Services
                     CategoryDateCreated = x.DateCreated,
                     Description = x.Description,
                 }).ToList();
-
-            //Put this on after ni Jule para sa Foreign Key Application
-            /*var retrievedData = _expenseRepository.RetrieveExpenses().Where(x=>x.UserId == userId).Join(_categoryRepository.RetrieveCategories(), 
-                expense => expense.CategoryId,
-                category = > category.CategoryId,
-                (expense,category) => new ExpenseViewModel
-                {
-                    ExpenseId = expense.ExpenseId,
-                    Title = expense.Title,
-                    Amount = expense.Amount,
-                    Description = expense.Description,
-                    CategoryId = category.CategoryId,
-                    CategoryName = category.CategoryName,
-                }).ToList();*/
-
             return retrievedData;
         }
 
