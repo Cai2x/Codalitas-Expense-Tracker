9996a8b5d947fe44752fe5de34616fc607288f2b
James <takashimaretubado@gmail.com>
2024-11-21 22:38:08 +0800

    [Jule][Change Password Implemented]

---
+++ ASI.Basecode.WebApp/Models/ChangePasswordModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASI.Basecode.WebApp.Models
+{
+    public class ChangePasswordModel
+    {
+        [Required(ErrorMessage = "Old Password is required.")]
+        public string OldPassword { get; set; }
+        [Required(ErrorMessage = "New Password is required.")]
+        public string NewPassword { get; set; }
+        [Required(ErrorMessage = "Confirm Password is required.")]
+        [Compare("NewPassword", ErrorMessage = "New Password and Confirmation Password must match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
